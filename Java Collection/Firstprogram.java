5e7d2fe44c9e2d41de34249830349ead37cb4274
rishavjha2000 <114063312+rishavjha2000@users.noreply.github.com>
2022-09-29 15:06:48 +0530

    Update Firstprogram.java

--- Java Collection/Firstprogram.java
+++ Java Collection/Firstprogram.java
@@ -1,3 +1,5 @@
+1) Write Java code to define List . Insert 5 floating point numbers in List, and using an iterator, find the sum of the numbers in List.
+
 import java.util.ArrayList;
 import java.util.List;
 
