79b0bf94158538f845fef219608fc7801268d3ea
rishavjha2000 <114063312+rishavjha2000@users.noreply.github.com>
2022-10-02 20:17:36 +0530

    Add files via upload

---
+++ SecondQuest.java
@@ -0,0 +1,34 @@
+/*Q2) Using (instance) Method reference create and apply add and subtract method and using (Static)
+ Method reference create and apply multiplication method for the functional interface created */
+
+public class SecondQuest {
+
+    static void display() {
+        int a = 5;
+        int b = 10;
+        int mul = a * b;
+        System.out.println("Static method returns multiplication:" + mul);
+    }
+
+    void display2() {
+        int a = 30;
+        int b = 20;
+        int sum = a + b;
+        int sub = a - b;
+        System.out.println("Instance add method returns sum:" + sum + "and returns substraction:" + sub);
+
+    }
+
+    public static void main(String[] args) {
+        MethodReferenceInterface methodreferenceinterface = SecondQuest::display;
+        methodreferenceinterface.print();
+
+        SecondQuest methodreference = new SecondQuest();
+        MethodReferenceInterface methodreferenceinterface1 = methodreference::display2;
+        methodreferenceinterface1.print();
+    }
+}
+
+interface MethodReferenceInterface {
+    void print();
+}
