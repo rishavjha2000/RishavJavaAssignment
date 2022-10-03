aa112efd696c708feff631a066e359a11ecc1732
rishavjha2000 <114063312+rishavjha2000@users.noreply.github.com>
2022-10-03 14:03:15 +0530

    Add files via upload

---
+++ java 8/FifthQuest.java
@@ -0,0 +1,22 @@
+/* Q5)Implement multiple inheritance with default method inside interface. */
+
+interface Clickable {
+    default void click() {
+        System.out.println("click");
+    }
+}
+
+interface Accessible {
+    default void access() {
+        System.out.println("access");
+    }
+}
+
+public class FifthQuest implements Clickable, Accessible {
+
+    public static void main(String[] args) {
+        FifthQuest button = new FifthQuest();
+        button.click();
+        button.access();
+    }
+}
---
+++ java 8/FourthQuest.java
@@ -0,0 +1,22 @@
+/* Q4) WAP using java 8:
+
+Collect all  even numbers from a list using stream
+Given a list of objects of following class:
+           class Employee{
+           String fullName;
+           Long salary;
+           String city;
+           }
+
+          Get list of all unique firstNames of employees where their salary is less than 5000 and who live in delhi.
+          Note: Full name is concatenation of first name, middle name and last name with single space in between */
+
+ import java.util.Arrays;
+import java.util.List;
+
+public class FourthQuest {
+
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        numbers.stream().filter(value -> value % 2 == 0).forEach(System.out::println);
+    }
